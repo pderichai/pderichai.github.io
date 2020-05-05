89f91decee78344cceb1fbfc05c63c7176f69760
Deric Pang <pangd@google.com>
2020-05-05 03:35:06 -0700

    Faded

--- js/main.js
+++ js/main.js
@@ -6,18 +6,25 @@
   window.addEventListener('load', init);
 
   function init(){
-    let previouslyContent = document.getElementById("previously-content");
-    previouslyContent.children[previouslyIdx].style.display = "block";
+    let previouslyContent = document.getElementById("previously-blurbs");
+    previouslyContent.children[previouslyIdx].style.display = "inline";
+    previouslyContent.children[previouslyIdx].style.opacity = 1;
 
-    let previouslyButton = document.getElementById("previously-button");
-    previouslyButton.addEventListener("click", showNextPreviouslyContent);
+    window.setInterval(showNextPreviouslyContent, 5000);
   }
 
+  // TODO: This is really awful. Fix this.
   function showNextPreviouslyContent() {
-    let previouslyContent = document.getElementById("previously-content");
-    previouslyContent.children[previouslyIdx].style.display = "none";
-    previouslyIdx = (previouslyIdx + 1) % previouslyContent.childElementCount;
-    previouslyContent.children[previouslyIdx].style.display = "block";
+    let previouslyContent = document.getElementById("previously-blurbs");
+    previouslyContent.children[previouslyIdx].style.opacity = 0;
+    window.setTimeout(function () {
+        previouslyContent.children[previouslyIdx].style.display = "none";
+        previouslyIdx = (previouslyIdx + 1) % previouslyContent.childElementCount;
+        previouslyContent.children[previouslyIdx].style.display = "inline";
+        window.setTimeout(function () {
+          previouslyContent.children[previouslyIdx].style.opacity = 1;
+        }, 500);
+      }, 1000);
   }
 
 })();

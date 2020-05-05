fe2e190ed8c05779b22e59ef0a22c7af6a2fc324
Deric Pang <pangd@google.com>
2020-05-05 15:43:28 -0700

    Text fading in JS.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 "use strict";
 (function() {
 
+  const FADE_STEPS = 500;
+  const PREVIOUSLY_CHANGE_FREQ = 5000;
+
   var previouslyIdx = 0;
+  var start = null;
 
   window.addEventListener('load', init);
 
@@ -10,21 +14,42 @@
     previouslyContent.children[previouslyIdx].style.display = "inline";
     previouslyContent.children[previouslyIdx].style.opacity = 1;
 
-    window.setInterval(showNextPreviouslyContent, 5000);
+    window.setInterval(showNextPreviouslyContent, PREVIOUSLY_CHANGE_FREQ);
   }
 
-  // TODO: This is really awful. Fix this.
   function showNextPreviouslyContent() {
+    if (document.hasFocus()) {
+      let previouslyContent = document.getElementById("previously-blurbs");
+      window.requestAnimationFrame(fadeOutTextStep);
+    }
+  }
+
+  function fadeOutTextStep(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
     let previouslyContent = document.getElementById("previously-blurbs");
-    previouslyContent.children[previouslyIdx].style.opacity = 0;
-    window.setTimeout(function () {
-        previouslyContent.children[previouslyIdx].style.display = "none";
-        previouslyIdx = (previouslyIdx + 1) % previouslyContent.childElementCount;
-        previouslyContent.children[previouslyIdx].style.display = "inline";
-        window.setTimeout(function () {
-          previouslyContent.children[previouslyIdx].style.opacity = 1;
-        }, 500);
-      }, 1000);
+    previouslyContent.children[previouslyIdx].style.opacity = 1 - (progress / FADE_STEPS);
+    if (progress < FADE_STEPS) {
+      window.requestAnimationFrame(fadeOutTextStep);
+    } else {
+      start = null;
+      previouslyContent.children[previouslyIdx].style.display = "none";
+      previouslyIdx = (previouslyIdx + 1) % previouslyContent.childElementCount;
+      previouslyContent.children[previouslyIdx].style.display = "inline";
+      window.requestAnimationFrame(fadeInTextStep);
+    }
+  }
+
+  function fadeInTextStep(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    let previouslyContent = document.getElementById("previously-blurbs");
+    previouslyContent.children[previouslyIdx].style.opacity = progress / FADE_STEPS;
+    if (progress < FADE_STEPS) {
+      window.requestAnimationFrame(fadeInTextStep);
+    } else {
+      start = null;
+    }
   }
 
 })();

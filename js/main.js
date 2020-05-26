a96f1e8a308cac2ad606d4c9a34633657acbfdf9
Deric Pang <pderichai@gmail.com>
2020-05-26 13:05:18 -0700

    Minor JS cleanup.

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@
   const FADE_STEPS = 500;
   const PREVIOUSLY_CHANGE_FREQ = 5000;
 
-  var previouslyIdx = 0;
+  var previouslyContentIndex = 0;
   var start = null;
 
   window.addEventListener('load', init);
 
-  function init(){
+  function init() {
     let previouslyContent = document.getElementById("previously-blurbs");
-    previouslyContent.children[previouslyIdx].style.display = "inline";
-    previouslyContent.children[previouslyIdx].style.opacity = 1;
+    previouslyContent.children[previouslyContentIndex].style.display = "inline";
+    previouslyContent.children[previouslyContentIndex].style.opacity = 1;
 
     window.setInterval(showNextPreviouslyContent, PREVIOUSLY_CHANGE_FREQ);
   }
@@ -28,14 +28,14 @@
     if (!start) start = timestamp;
     var progress = timestamp - start;
     let previouslyContent = document.getElementById("previously-blurbs");
-    previouslyContent.children[previouslyIdx].style.opacity = 1 - (progress / FADE_STEPS);
+    previouslyContent.children[previouslyContentIndex].style.opacity = 1 - (progress / FADE_STEPS);
     if (progress < FADE_STEPS) {
       window.requestAnimationFrame(fadeOutTextStep);
     } else {
       start = null;
-      previouslyContent.children[previouslyIdx].style.display = "none";
-      previouslyIdx = (previouslyIdx + 1) % previouslyContent.childElementCount;
-      previouslyContent.children[previouslyIdx].style.display = "inline";
+      previouslyContent.children[previouslyContentIndex].style.display = "none";
+      previouslyContentIndex = (previouslyContentIndex + 1) % previouslyContent.childElementCount;
+      previouslyContent.children[previouslyContentIndex].style.display = "inline";
       window.requestAnimationFrame(fadeInTextStep);
     }
   }
@@ -44,7 +44,7 @@
     if (!start) start = timestamp;
     var progress = timestamp - start;
     let previouslyContent = document.getElementById("previously-blurbs");
-    previouslyContent.children[previouslyIdx].style.opacity = progress / FADE_STEPS;
+    previouslyContent.children[previouslyContentIndex].style.opacity = progress / FADE_STEPS;
     if (progress < FADE_STEPS) {
       window.requestAnimationFrame(fadeInTextStep);
     } else {

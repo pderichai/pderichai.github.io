2f4d09c1a7a559223ba7263925956eba94e4ef55
Deric Pang <pderichai@gmail.com>
2020-05-26 19:07:06 -0700

    Tellina button javascript in js.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 "use strict";
-(function() {
-
+(function () {
   const FADE_STEPS = 500;
   const PREVIOUSLY_CHANGE_FREQ = 5000;
 
   var previouslyContentIndex = 0;
   var start = null;
 
-  window.addEventListener('load', init);
+  window.addEventListener("load", init);
 
   function init() {
     let previouslyContent = document.getElementById("previously-blurbs");
@@ -15,6 +14,9 @@
     previouslyContent.children[previouslyContentIndex].style.opacity = 1;
 
     window.setInterval(showNextPreviouslyContent, PREVIOUSLY_CHANGE_FREQ);
+
+    let tellinaSearchButton = document.getElementById("tellina-search-button");
+    tellinaSearchButton.addEventListener("click", searchTellina);
   }
 
   function showNextPreviouslyContent() {
@@ -28,14 +30,17 @@
     if (!start) start = timestamp;
     var progress = timestamp - start;
     let previouslyContent = document.getElementById("previously-blurbs");
-    previouslyContent.children[previouslyContentIndex].style.opacity = 1 - (progress / FADE_STEPS);
+    previouslyContent.children[previouslyContentIndex].style.opacity =
+      1 - progress / FADE_STEPS;
     if (progress < FADE_STEPS) {
       window.requestAnimationFrame(fadeOutTextStep);
     } else {
       start = null;
       previouslyContent.children[previouslyContentIndex].style.display = "none";
-      previouslyContentIndex = (previouslyContentIndex + 1) % previouslyContent.childElementCount;
-      previouslyContent.children[previouslyContentIndex].style.display = "inline";
+      previouslyContentIndex =
+        (previouslyContentIndex + 1) % previouslyContent.childElementCount;
+      previouslyContent.children[previouslyContentIndex].style.display =
+        "inline";
       window.requestAnimationFrame(fadeInTextStep);
     }
   }
@@ -44,7 +49,8 @@
     if (!start) start = timestamp;
     var progress = timestamp - start;
     let previouslyContent = document.getElementById("previously-blurbs");
-    previouslyContent.children[previouslyContentIndex].style.opacity = progress / FADE_STEPS;
+    previouslyContent.children[previouslyContentIndex].style.opacity =
+      progress / FADE_STEPS;
     if (progress < FADE_STEPS) {
       window.requestAnimationFrame(fadeInTextStep);
     } else {
@@ -52,4 +58,9 @@
     }
   }
 
+  function searchTellina() {
+    window.location.href =
+      "http://kirin.cs.washington.edu:8000/translate?request_str=" +
+      document.getElementById("tellina-search-bar").value;
+  }
 })();

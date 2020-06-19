2d1e8c0d90b282b30e3b92a64a7703641b0d5831
Deric Pang <pderichai@gmail.com>
2020-06-19 16:04:54 -0700

    Removing unused static query.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,21 +7,10 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import "./layout.scss"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <div>
       <main>{children}</main>

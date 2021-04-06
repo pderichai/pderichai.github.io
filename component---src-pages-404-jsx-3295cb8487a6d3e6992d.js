73e690b15dedc581be4fca47c408ab3413a182e9
pderichai <pderichai@users.noreply.github.com>
2021-04-06 18:53:23 +0000

    deployed via Gatsby Publish Action 🎩 for 70e30129e09914cbdbd7d6d814c59ddd16384622

---
+++ component---src-pages-404-jsx-3295cb8487a6d3e6992d.js
@@ -0,0 +1,2 @@
+(self.webpackChunkderic_homepage=self.webpackChunkderic_homepage||[]).push([[256],{1657:function(e,t,n){"use strict";n.r(t);var u=n(7294),l=n(6985),a=n(1277);t.default=function(){return u.createElement(l.Z,null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-3295cb8487a6d3e6992d.js.map
